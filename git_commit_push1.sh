7fe422ba4d8e7b1129f460aeb96e616d22acebf2
jg8481 <joshua.gorospe@gmail.com>
2016-09-01 22:03:51 -0400

    Delete git_commit_push1.sh

--- git_commit_push1.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-##REPO_DIR=/Users/joshuagorospe/GraphWalker_Test_Results2
-#cd ~/GraphWalker_Test_Results2
-git add .
-git commit -am "This is a result log update triggered by a QA Machine."
-git push origin
-#read
