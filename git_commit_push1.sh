c39c34cb8e30b6a76addeef0eb74c375103fc67d
Joshua Gorospe <joshua.gorospe@intersection.com>
2016-09-01 21:49:19 -0400

    This is a result log update triggered by a QA Machine.

--- git_commit_push1.sh
+++ git_commit_push1.sh
@@ -1,7 +1,7 @@
-#! /bin/bash
+#! /bin/sh
 ##REPO_DIR=/Users/joshuagorospe/GraphWalker_Test_Results2
 #cd ~/GraphWalker_Test_Results2
 git add .
 git commit -am "This is a result log update triggered by a QA Machine."
-git push https://jg8481:gorospe81@github.com/jg8481/GraphWalker_Test_Results2.git master
+git push https://jg8481:gorospe81@github.com/jg8481/GraphWalker_Test_Results2.git
 #read
